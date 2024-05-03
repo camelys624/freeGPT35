40ec153c6fca290b149e7d8b2b34d269d525c140
camelys <camel_yangz@163.com>
2024-05-04 00:40:40 +0800

    link mangodb

---
+++ db.py
@@ -0,0 +1,34 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+
+uri = f"mongodb+srv://{username}:{password}@cluster0.7whr0zh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client.bookkeeping
+collection = db.billing
+
+def insertData(data):
+    result = collection.insert_one(data)
+    
+    if result.acknowledged:
+        print("插入成功")
+    else:
+        print("插入失败")
+
+
+insertData({
+    "date": "2024-05-03",
+    "item": "groceries",
+    "cost": 15,
+    "description": "今天买菜花费15元"
+})
---
+++ main.py
@@ -0,0 +1,36 @@
+import g4f
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+g4f.logging = True
+
+
+@app.get('/')
+def spin_content(content):
+    print(content)
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.default,
+        provider=None,
+        messages=[
+            {"role": "user", "content": """现在你是我的记账助手，你需要根据我的提示，将其转换为此格式 ```json
+                {
+                    "date": Date,
+                    "item": String,
+                    "cost": Number,
+                    "description": String
+                }
+    ```的纯json字符串，不要添加任何描述和其他的东西。"""},
+            {"role": "user", "content": content}
+        ]
+    )
+
+    return response
